23697d96fcdf6d0ca3791f6e91cccfc6f41a7bf3
mahmudmoon2 <mahmudmoon2@gmail.com>
2026-01-04 22:28:08 +0600

    Updated models and added managers

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from accounts.managers import CustomUserManager
 
 # Create your models here.
 
@@ -17,6 +18,8 @@ class CustomUser(AbstractUser):
     profile_picture = models.ImageField(null=True, blank= True, upload_to = "user_profile")
     
     username = None
+    
+    objects = CustomUserManager()
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
